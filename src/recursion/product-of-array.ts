3876218c23fd82cd8f336fd10960447593648114
madhank93 <madhankumaravelu93@gmail.com>
2022-03-28 22:27:41 +0530

    fix: handle empty array early and return zero

--- src/recursion/product-of-array.ts
+++ src/recursion/product-of-array.ts
@@ -7,11 +7,13 @@ import { expect } from "chai";
  * @returns number
  */
 function productOfArray(arr: number[]): number {
-  if (arr.length === 0) return 1;
+  if (arr.length === 0) return 0;
+  else if (arr.length === 1) return arr[0];
   return arr[0] * productOfArray(arr.slice(1));
 }
 
 expect(productOfArray([1, 2, 3])).to.equal(6);
 expect(productOfArray([1, 2, 3, 10])).to.equal(60);
 expect(productOfArray([1, 2, 0])).to.equal(0);
-expect(productOfArray([])).to.equal(1);
+expect(productOfArray([5])).to.equal(5);
+expect(productOfArray([])).to.equal(0);
