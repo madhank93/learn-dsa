0661a2efdaa866d32403f7bb3eda9547c2ffa0dc
madhank93 <madhankumaravelu93@gmail.com>
2022-07-24 17:30:25 +0530

    test: adding more tests

--- src/data-structures/single-linked-list/single-linked-list.spec.ts
+++ src/data-structures/single-linked-list/single-linked-list.spec.ts
@@ -4,9 +4,8 @@ import SingleLinkedList from "./single-linked-list";
 describe("Single linked list test", () => {
   it("#addToBack, Add single node to the list", () => {
     let sll = new SingleLinkedList();
-
-    // Adding first node
     sll.addToBack(5);
+
     expect(sll.head?.value).toBe(5);
     expect(sll.tail?.value).toBe(5);
     expect(sll.head?.next).toBe(null);
@@ -17,6 +16,7 @@ describe("Single linked list test", () => {
     sll.addToBack(5);
     sll.addToBack(6);
     sll.addToBack(7);
+
     expect(sll.head?.value).toBe(5);
     expect(sll.head?.next?.value).toBe(6);
     expect(sll.tail?.value).toBe(7);
@@ -25,18 +25,19 @@ describe("Single linked list test", () => {
   it("#remove, Remove an existing node from the list should return true", () => {
     let sll = new SingleLinkedList();
     sll.addToBack(10);
+
     expect(sll.remove(10)).toBeTruthy();
   });
 
   it("#remove, Remove an non-existing element from the list should return false", () => {
     let sll = new SingleLinkedList();
     sll.addToBack(10);
+
     expect(sll.remove(1)).toBeFalsy();
   });
 
   it("#remove, Remove head (first) node from the list", () => {
     let sll = new SingleLinkedList();
-
     sll.addToBack(5);
     sll.addToBack(7);
     sll.addToBack(8);
@@ -47,8 +48,6 @@ describe("Single linked list test", () => {
 
   it("#remove, Remove mid node from the list", () => {
     let sll = new SingleLinkedList();
-
-    // Remove mid node
     sll.addToBack(5);
     sll.addToBack(7);
     sll.addToBack(8);
@@ -67,5 +66,39 @@ describe("Single linked list test", () => {
     sll.remove(9);
 
     expect(sll.tail?.value).toBe(8);
+  });
+
+  it("#insertAfter, Insert a node in the middle after the search node", () => {
+    let sll = new SingleLinkedList();
+    sll.addToBack(10);
+    sll.addToBack(12);
+    sll.insertAfter(10, 11);
+
+    expect(sll.head?.next?.value).toBe(11);
+  });
+
+  it("#insertAfter, Insert a node after the tail", () => {
+    let sll = new SingleLinkedList();
+    sll.addToBack(10);
+    sll.addToBack(20);
+    sll.insertAfter(20, 30);
+
+    expect(sll.tail?.value).toBe(30);
+  });
+
+  it("#insertAfter, Should return true if a node is found", () => {
+    let sll = new SingleLinkedList();
+    sll.addToBack(10);
+    sll.addToBack(20);
+
+    expect(sll.insertAfter(20, 30)).toBeTruthy();
+  });
+
+  it("#insertAfter, Should return false if a node is not found", () => {
+    let sll = new SingleLinkedList();
+    sll.addToBack(10);
+    sll.addToBack(20);
+
+    expect(sll.insertAfter(99, 30)).toBeFalsy();
   });
 });
