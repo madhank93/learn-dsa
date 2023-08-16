0cd77ab8373e3defe111f66a3daf5012fd45b78f
Madhan <madhankumaravelu93@gmail.com>
2023-08-15 23:09:11 -0500

    feat: valid sub sequence

---
+++ src/problems/algoexpert/02-validate-subsequence.ts
@@ -0,0 +1,20 @@
+function isValidSubsequence(array: number[], sequence: number[]) {
+    let seqIndex = 0;
+    for (const value of array) {
+        if (seqIndex === sequence.length) {
+            break;
+        }
+        if(value === sequence[seqIndex]) {
+            seqIndex++;
+        }
+    }
+    return seqIndex === sequence.length
+  }
+
+  if (import.meta.vitest) {
+    const { it, expect } = import.meta.vitest;
+    it('Verify given `array` has valid `sub-sequence`', () => {
+      expect(isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10])).toBeTruthy()
+    });
+  }
+  
