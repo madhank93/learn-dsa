ad4807b000bad3e74b21b9a850e3865419af93a6
Madhan <madhankumaravelu93@gmail.com>
2023-08-16 22:23:06 -0500

    test: adding more test cases

--- src/problems/algoexpert/02-validate-subsequence.ts
+++ src/problems/algoexpert/02-validate-subsequence.ts
@@ -15,6 +15,9 @@ function isValidSubsequence(array: number[], sequence: number[]) {
     const { it, expect } = import.meta.vitest;
     it('Verify given `array` has valid `sub-sequence`', () => {
       expect(isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10])).toBeTruthy()
+      expect(isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, 6])).toBeFalsy()
+      expect(isValidSubsequence([5, 1, 22, 6, 6, -1, 8, 10], [1, 6, -1, 10])).toBeTruthy()
+      expect(isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [5, 1, 22, 25, 6, -1, 8, 10])).toBeTruthy()
     });
   }
   
