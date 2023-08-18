7b739360e57c08987d0113ed9d24397e278ae693
Madhan <madhankumaravelu93@gmail.com>
2023-08-17 19:31:11 -0500

    chore: adding ques

--- src/problems/algoexpert/02-validate-subsequence.ts
+++ src/problems/algoexpert/02-validate-subsequence.ts
@@ -1,3 +1,18 @@
+/**
+  Given two non-empty arrays of integers, write a function that determines whether the second array is 
+  a subsequence of the first one.
+
+  A subsequence of an array is a set of numbers that aren't necessarily adjacent in the array but that 
+  are in the same order as they appear in the array. For instance, the numbers.
+
+  Sample input:
+  array  = [5, 1, 22, 25, 6, -1, 8, 10]
+  sequence = [1, 6, -1, 10]
+
+  Sample Output:
+  true
+ */
+
 function isValidSubsequence(array: number[], sequence: number[]) {
     let seqIndex = 0;
     for (const value of array) {
