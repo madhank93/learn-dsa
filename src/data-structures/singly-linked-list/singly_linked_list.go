a7e7a6d9590db2a3055ad3b6f3b85c4e8c70dc20
Madhan <madhankumaravelu93@gmail.com>
2024-04-09 18:02:09 -0500

    feat: added insert, delete and print functions

--- src/data-structures/singly-linked-list/singly_linked_list.go
+++ src/data-structures/singly-linked-list/singly_linked_list.go
@@ -12,7 +12,7 @@ type SinglyLinkedList struct {
 	tail *Node
 }
 
-func (sll *SinglyLinkedList) add(data int) {
+func (sll *SinglyLinkedList) insert(data int) {
 	newNode := &Node{value: data, next: nil}
 	if sll.head == nil {
 		sll.head = newNode
@@ -23,11 +23,26 @@ func (sll *SinglyLinkedList) add(data int) {
 	}
 }
 
-func (sll *SinglyLinkedList) printValues() {
+func (sll *SinglyLinkedList) delete(data int) {
+	previousNode := sll.head
+	searchNode := sll.head
+
+	for searchNode != nil {
+		if searchNode.value == data {
+			previousNode.next = searchNode.next
+		}
+		previousNode = searchNode
+		searchNode = searchNode.next
+	}
+}
+
+func (sll *SinglyLinkedList) print() {
 	current := sll.head
 	for current != nil {
 		fmt.Print(current.value)
-		fmt.Printf(" -> ")
+		if current.next != nil {
+			fmt.Printf(" -> ")
+		}
 		current = current.next
 	}
 }
